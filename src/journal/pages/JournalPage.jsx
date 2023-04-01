8e8752ea60e046780fc09d9bc423301909e33dfa
Guillermo <memolvpz@gmail.com>
2023-04-01 17:52:59 -0600

    NothingSelectedView created

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,12 +1,10 @@
-import { Typography } from "@mui/material"
 import { JournalLayout } from "../layout/JournalLayout"
+import { NothingSelectedView } from "../views"
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      <Typography>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit at tenetur, quos facere eligendi tempora! Laboriosam mollitia saepe repellat, voluptatum corrupti aliquam accusantium perferendis quisquam soluta porro doloribus minima voluptates!
-      </Typography>
+      <NothingSelectedView />
     </JournalLayout>
   )
 }
