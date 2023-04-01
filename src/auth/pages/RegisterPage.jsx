e33b47e9f5fd8e14cf9c902c1218350ab2fd79bf
Guillermo <memolvpz@gmail.com>
2023-04-01 15:48:25 -0600

    RegisterPage Layout completed

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,5 +1,54 @@
+import { Link as RouterLink } from 'react-router-dom'
+import { Google } from "@mui/icons-material"
+import { Button, Grid, Link, TextField, Typography } from "@mui/material"
+import { AuthLayout } from '../layout/AuthLayout'
+
 export const RegisterPage = () => {
   return (
-    <div>RegisterPage</div>
+    <AuthLayout title='Create an account'>
+      <form>
+            <Grid container>
+              <Grid item xs={ 12 } sx={{ mt: 2 }}>
+                <TextField 
+                  label="Full name" 
+                  type="text" 
+                  placeholder="Guillermo Olvera" 
+                  fullWidth
+                />
+              </Grid>
+              <Grid item xs={ 12 } sx={{ mt: 2 }}>
+                <TextField 
+                  label="Email" 
+                  type="email" 
+                  placeholder="correo@ejemplo.com" 
+                  fullWidth
+                />
+              </Grid>
+              <Grid item xs={ 12 } sx={{ mt: 2 }}>
+                <TextField 
+                  label="Password" 
+                  type="password" 
+                  placeholder="Password" 
+                  fullWidth
+                />
+              </Grid>
+
+              <Grid container spacing={ 2 } sx={{ mb: 2, mt: 1 }}>
+                <Grid item xs={ 12 }>
+                  <Button variant="contained" fullWidth>
+                    Create Account
+                  </Button>
+                </Grid>
+              </Grid>
+
+              <Grid container direction='row' justifyContent='end'>
+                <Typography xs={{ mr: 1 }}>Already have an account?</Typography>
+                <Link component={ RouterLink } color='inherit' to='/auth/login'>
+                  Login
+                </Link>
+              </Grid>
+            </Grid>
+        </form>
+    </AuthLayout>
   )
 }
