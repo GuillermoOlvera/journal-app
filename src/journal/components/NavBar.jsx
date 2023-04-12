35e78a9f62044fad887f640dec152e208ed21e00
Guillermo <memolvpz@gmail.com>
2023-04-11 22:54:05 -0600

    animations added

--- src/journal/components/NavBar.jsx
+++ src/journal/components/NavBar.jsx
@@ -1,7 +1,16 @@
 import { LogoutOutlined, MenuOutlined } from "@mui/icons-material"
 import { AppBar, Grid, IconButton, Toolbar, Typography } from "@mui/material"
+import { useDispatch } from "react-redux";
+import { startLogout } from "../../store/auth";
 
 export const NavBar = ({ drawerWidth = 240 }) => {
+
+    const dispatch = useDispatch();
+
+    const onLogout = () => {
+        dispatch(startLogout());
+    };
+
   return (
     <AppBar 
         position="fixed" 
@@ -30,7 +39,10 @@ export const NavBar = ({ drawerWidth = 240 }) => {
                 >
                     Journal App
                 </Typography>
-                <IconButton color='error'>
+                <IconButton 
+                    color='error'
+                    onClick={ onLogout }
+                >
                     <LogoutOutlined />
                 </IconButton>
             </Grid>
