7109cbcb5c89e0ce852bc589f1859f1d06721134
Guillermo <memolvpz@gmail.com>
2023-04-12 22:10:53 -0600

    startNewNote thunk created

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -2,14 +2,24 @@ import { AddOutlined } from "@mui/icons-material"
 import { IconButton } from "@mui/material"
 import { JournalLayout } from "../layout/JournalLayout"
 import { NoteView, NothingSelectedView } from "../views"
+import { startNewNote } from "../../store/journal/thunks"
+import { useDispatch } from "react-redux"
 
 export const JournalPage = () => {
+
+  const dispatch = useDispatch();
+
+  const onClickNewNote = () => {
+    dispatch(startNewNote());
+  }
+
   return (
     <JournalLayout>
       <NothingSelectedView />
       {/* <NoteView /> */}
 
       <IconButton
+        onClick={onClickNewNote}
         size='large'
         sx={{
           color: 'white',
